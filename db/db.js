0d31cdb258bb5c2963518cdea6958f10eea09a98
Faizan <fa09042003@gmail.com>
2023-11-03 21:52:51 +0530

    first commit

---
+++ db/db.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+const db = async () => {
+    try {
+        mongoose.set('strictQuery', false);
+        await mongoose.connect(process.env.MONGO_URL);
+        console.log('Db Connected');
+    } catch (error) {
+        console.log('DB Connection Error');
+    }
+}
+
+module.exports = {db};
